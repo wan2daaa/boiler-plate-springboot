03bb5df10a168b7ff85f42651bb7b12bf0ff11ce
wan2daaa <wan2daaa@gmail.com>
2023-09-04 16:56:03 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/me/wane/boilerplatespringboot/BoilerPlateSpringbootApplication.java
@@ -0,0 +1,13 @@
+package me.wane.boilerplatespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerPlateSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoilerPlateSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/wane/boilerplatespringboot/BoilerPlateSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.wane.boilerplatespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerPlateSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
